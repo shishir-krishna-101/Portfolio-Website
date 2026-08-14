6006db6e9c3a6a98059f631d57b4d62c36eebdd0
shishir-krishna-101 <shishirkrishna20@gmail.com>
2026-08-14 21:04:39 +0530

    Initial commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>K R Shishir Krishna</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">DevOps Engineer</h5>
         <CTA />
         <HeaderSocial/>
         <div className="me">
