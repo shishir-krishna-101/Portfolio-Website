945d2e94b5e79494e1577f6e93531d88804e0885
shishir-krishna-101 <shishirkrishna20@gmail.com>
2026-08-15 23:05:58 +0530

    Remove duplicate import statements in Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import "./nav.css";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
 import { BsSun, BsMoon } from "react-icons/bs";
 import { RiServiceLine } from "react-icons/ri";
