b8c6c59fccd1824d2f1b2dceb8ce539a60aaddcb
shishir-krishna-101 <shishirkrishna20@gmail.com>
2026-08-15 14:57:09 +0530

    Change LinkedIn link to specific user profile
    Updated LinkedIn link to a specific profile.

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import { FiDribbble } from "react-icons/fi";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank" rel="noreferrer" >
+      <a href="https://www.linkedin.com/in/shishir-krishna-a88a9ab9/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BoQQNNfAjSQOif6MjT7V1Pw%3D%3D" target="_blank" rel="noreferrer" >
         <BsLinkedin />
       </a>
       <a href="https://github.com" target="_blank" rel="noreferrer">
