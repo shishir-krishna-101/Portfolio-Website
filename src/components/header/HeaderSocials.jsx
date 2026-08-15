445427338a50eb538da69e8fe67499cf065e08f8
shishir-krishna-101 <shishirkrishna20@gmail.com>
2026-08-15 14:59:08 +0530

    Update GitHub link in HeaderSocials component

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,7 +9,7 @@ const HeaderSocials = () => {
       <a href="https://www.linkedin.com/in/shishir-krishna-a88a9ab9/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BoQQNNfAjSQOif6MjT7V1Pw%3D%3D" target="_blank" rel="noreferrer" >
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank" rel="noreferrer">
+      <a href="https://github.com/shishir-krishna-101" target="_blank" rel="noreferrer">
         <FaGithub />
       </a>
       <a href="https://dribble.com" target="_blank" rel="noreferrer">
